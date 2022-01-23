4ee2e04e8bc98afc294dfe6cb24373fc8591348c
polatengin <enpolat@microsoft.com>
2022-01-22 21:28:23 -0800

    adding configure elements

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,33 @@ export const App: FunctionComponent = () => {
 
         <div className="p-4 rounded shadow bg-white flex gap-4">
 
+          <div className="w-1/2">
+            <label className="text-gray-600 font-bold pr-4">
+              Start calendar from
+            </label>
+            <div className="flex">
+              <div className="w-2/3">
+                <label className="text-gray-600 font-bold pr-4">
+                  Year
+                </label>
+              </div>
+              <div className="w-1/3">
+                <input className="form-input block w-full border" type="number" value="" />
+              </div>
+            </div>
+
+            <div className="flex">
+              <div className="w-2/3">
+                <label className="text-gray-600 font-bold pr-4">
+                  Month
+                </label>
+              </div>
+              <div className="w-1/3">
+                <input className="form-input block w-full border" type="number" value="" />
+              </div>
+            </div>
+          </div>
+
         </div>
       </section>
     </div>
