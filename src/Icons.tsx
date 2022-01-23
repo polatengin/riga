a38895549c8d9ef72c1ad2a3b6678d8ee0124145
polatengin <enpolat@microsoft.com>
2022-01-22 21:23:42 -0800

    adding logo icon

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -5,3 +5,11 @@ interface IconProps {
   onClick?: () => void;
 }
 
+export const LogoIcon: FunctionComponent<IconProps> = (props: IconProps) => {
+  return (
+    <svg viewBox="0 0 1200 600" fill="none" className={props.className} onClick={props.onClick}>
+      <path d="m0,0h1200v600H0" />
+      <path fill="#fff" d="m0,200h1200v200H0" />
+    </svg>
+  );
+};
