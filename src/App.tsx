90454f762c06f9a6757d11129b5e59241ae7f131
polatengin <enpolat@microsoft.com>
2022-05-09 14:36:37 -0700

    fixing styling issue

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export const App: FunctionComponent = () => {
   const [month, setMonth] = useState(new Date().getMonth() + 1);
   const [year, setYear] = useState(new Date().getFullYear());
   return (
-    <div className="bg-gray-100 text-gray-900 tracking-wider leading-normal h-screen flex flex-col items-center">
+    <div className="bg-gray-100 text-gray-900 tracking-wider leading-normal h-full flex flex-col items-center">
       <header className="bg-white shadow flex align-center p-4 container">
         <LogoIcon className="bg-red-500 w-12 h-8 mr-2 rounded" />
         <h1 className="text-xl text-gray-900">Riga App</h1>
