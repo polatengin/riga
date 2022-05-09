9d6f6f30e454c0ed546966874ac301ef13af6749
polatengin <enpolat@microsoft.com>
2022-05-09 14:33:57 -0700

    adding number of days in each month

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,11 @@
 export const Months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 export const WeekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+export const NumberOfDays = (year: number) => {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    return [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  }
+
+  return [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+};
