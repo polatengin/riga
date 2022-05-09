83da67d91fb347e4fc4196796ed32be23ac9a3b0
polatengin <enpolat@microsoft.com>
2022-05-09 14:34:37 -0700

    adding helper function that returns the matrix of day numbers for the given month

--- src/Utils.ts
+++ src/Utils.ts
@@ -9,3 +9,37 @@ export const NumberOfDays = (year: number) => {
 
   return [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 };
+
+export const GetCalendarMatrix = (month: number, year: number) => {
+  const firstDay = new Date(year, month, 1).getDay();
+  const maxDays = NumberOfDays(year)[month];
+
+  const matrix: string[][] = [];
+  matrix[0] = WeekDays;
+
+  let counter = 1;
+  let row = 1;
+
+  matrix.push([]);
+  for (let i = 0; i < firstDay; i++) {
+    matrix[row].push("");
+    counter++;
+  }
+  for (let i = 1; i <= maxDays; i++) {
+    matrix[row].push(i.toString());
+    counter++;
+    if (counter > 7) {
+      counter = 1;
+      row++;
+      matrix.push([]);
+    }
+  }
+  const c = matrix[row].length;
+  if (c > 0) {
+    for (let i = 0; i < 7 - c; i++) {
+      matrix[row].push("");
+    }
+  }
+
+  return matrix;
+}
