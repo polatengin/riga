798879d1354a048aaaa10d81540e60265eddf141
polatengin <enpolat@microsoft.com>
2022-05-09 14:33:42 -0700

    adding list of weeks

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,1 +1,3 @@
 export const Months = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+export const WeekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
