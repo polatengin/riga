031a42680ca7f24c33c58a3f4f5f75db21da4eb3
polatengin <enpolat@microsoft.com>
2022-05-09 14:35:59 -0700

    adding usestate into the imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import { LogoIcon } from './Icons';
 
 export const App: FunctionComponent = () => {
