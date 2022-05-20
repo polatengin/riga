80c7849c38347ba6087233d6510cf6286b8ddf1e
polatengin <enpolat@microsoft.com>
2022-05-20 11:12:34 -0700

    calculating next month info

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -10,6 +10,7 @@ interface CalendarProps {
 export const Calendar: FunctionComponent<CalendarProps> = (props: CalendarProps) => {
 
   const month = props.month;
+  const nextMonth = month + 1 > 12 ? 1 : month + 1;
   return (
     <div className="bg-white shadow p-4 select-none">
     </div>
