ee184cd4cbbbcb25d86284cb04ee52d9132a68ac
polatengin <enpolat@microsoft.com>
2022-05-20 11:13:22 -0700

    rendering next month/year on screen

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -17,6 +17,9 @@ export const Calendar: FunctionComponent<CalendarProps> = (props: CalendarProps)
     <div className="bg-white shadow p-4 select-none">
       <header className="flex align-items mb-6">
         <h3 className="text-6xl flex-grow">{Months[month]} {props.year}</h3>
+        <div className="w-36">
+          <h4 className="text-xs">{Months[nextMonth]} {nextYear}</h4>
+        </div>
       </header>
     </div>
   );
