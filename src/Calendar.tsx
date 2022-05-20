4e0c147358ab3193d241954c9ff8cafb502fb2d7
polatengin <enpolat@microsoft.com>
2022-05-20 11:12:40 -0700

    calculating next year info

--- src/Calendar.tsx
+++ src/Calendar.tsx
@@ -11,6 +11,8 @@ export const Calendar: FunctionComponent<CalendarProps> = (props: CalendarProps)
 
   const month = props.month;
   const nextMonth = month + 1 > 12 ? 1 : month + 1;
+  const nextYear = month + 1 > 12 ? props.year + 1 : props.year;
+
   return (
     <div className="bg-white shadow p-4 select-none">
     </div>
