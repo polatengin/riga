2b8d907da3533727743e2265b2854c84d3e4e3c8
polatengin <enpolat@microsoft.com>
2022-05-20 11:10:01 -0700

    get month information from props

--- src/CalendarMonth.tsx
+++ src/CalendarMonth.tsx
@@ -7,6 +7,7 @@ interface CalendarMonthProps {
 
 export const CalendarMonth: FunctionComponent<CalendarMonthProps> = (props: CalendarMonthProps) => {
 
+  const month = props.month - 1;
   return (
     <div className="calendar-month grid grid-cols-7 border">
     </div>
