85ae6d6a3a3b7af0a71ccaa8052abce37926c3d6
polatengin <enpolat@microsoft.com>
2022-05-20 11:11:19 -0700

    adding new cell for each day from the month matrix

--- src/CalendarMonth.tsx
+++ src/CalendarMonth.tsx
@@ -15,6 +15,15 @@ export const CalendarMonth: FunctionComponent<CalendarMonthProps> = (props: Cale
 
   return (
     <div className="calendar-month grid grid-cols-7 border">
+      { matrix.map((row, rowIndex) => {
+        return row.map((item, colIndex) => {
+          return (
+            <div className="border-t border-l p-1 h-16 flex content-start" key={`${rowIndex}-${colIndex}`} style={{ color: colIndex === 0 ? '#a00' : '#000' }}>
+              {item}
+            </div>
+          );
+        });
+      }) }
     </div>
   );
 
